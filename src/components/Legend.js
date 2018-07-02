90eb3f6f023f6cafa9ff6538c33fcca01a395401
David Burgess <david@spirito.us>
2018-07-02 13:00:08 -0400

    Edit logic is now handled in Legend and removed from LegendItem.  LegendItem appropriately only deals with props and no longer manages state.

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -2,27 +2,77 @@ import React, { Component } from 'react';
 import LegendItem from './LegendItem.js';
 
 class Legend extends Component {
-  state = {legendItems:[{title:"Test"}]}
+  state = {legendItems:[{id:Date.now(), title:"Test", description:"Testing this thing out",color:{r:200,g:20,b:20,a:.5}}]}
 
-  deleteLegendItem(event){
-    console.log(event);
+  deleteLegendItem = (toDelete)=>{
+    this.setState({ legendItems:  this.state.legendItems.filter(item=>item.id!=toDelete.id)})
   }
-  render() { 
+  addLegendItem = (event)=>{
     const {legendItems} = this.state;
+
+    this.setState({ 
+      "legendItems": 
+        [...this.state.legendItems, 
+        {title:"Item"+ legendItems.length, color:{r:20,g:200,b:20,a:.5}, id:Date.now()}
+      ]
+    })
+  }
+  updateMainLegendItem = ({title,description})=>{
+    this.setState({mainTitle:title, mainDescription: description});
+  }
+  updateLegendItem = ({id, title,description})=>{
+    const {legendItems} = this.state;
+    const updatedItems = legendItems
+      .map((li)=>{
+        if(id && li.id === id){
+          return {...li, title, description, id}
+        }
+
+        return li; 
+      });
+    this.setState({"legendItems" : updatedItems });
+  }
+
+  render() { 
+    const {mainTitle, mainDescription, legendItems} = this.state;
     return (  
       <div className = "legend" {...this.props.styles} style={{padding:"0px 20px"}}>
-        <LegendItem title="Web Component" description="testing this" color="false" bottomBorder="true"></LegendItem>
+        <LegendItem 
+          title={mainTitle || "Web Component Title"} 
+          description={mainDescription || "Add a description"} 
+          bottomborder="true"
+          update={this.updateMainLegendItem}
+        >
+        </LegendItem>
         {
           legendItems.map((li,i)=>{
             return (
-            <div key={li.title+i} style={{position:"relative"}}>
-              <LegendItem {...li} style={{display:"inline-block"}}></LegendItem>
-              <div style={{display:"inline-block", verticalAlign:"top",cursor:"pointer", margin:5}} onClick={()=>this.setState({ legendItems:  legendItems.filter(item=>item.title!=li.title)})}>x</div>
+            <div key={li.id} style={{position:"relative"}}>
+              <LegendItem 
+                {...li}
+                key={li.id}
+                style={{display:"inline-block"}}
+                update={this.updateLegendItem}
+              ></LegendItem>
+              {true &&
+                <div 
+                  style={{
+                    display:"inline-block", 
+                    verticalAlign:"top",
+                    cursor:"pointer", 
+                    margin:5
+                  }} 
+                  onClick={this.deleteLegendItem.bind(this,li)}>x</div>
+              }
             </div>
             )
           })
         }
-        <div  style={{cursor:"pointer"}}onClick={()=>this.setState({ "legendItems": this.state.legendItems.concat({title:"Item"+this.state.legendItems.length})})}>+</div>
+        <div  
+          style={{cursor:"pointer"}}
+          onClick={this.addLegendItem}>
+          +
+        </div>
       </div>
     )
   }
--- src/components/LegendItem.js
+++ src/components/LegendItem.js
@@ -4,34 +4,62 @@ import React, { Component } from 'react';
 import './LegendItem.css';
 
 class LegendItem extends Component {
-  state = {
-    title:"Title", 
-    description:"Describe this.",
-    displayColorPicker: false,
-    color:{r:100,g:1,b:1,a:1.0},
-    ... this.props
-  };
-
-
   handleTitleChange = (event)=>{
-    this.setState({"title":this.titleInput.value});
+    this.props.update({
+      id: this.props.id,
+      title:this.titleInput.value,
+      description:this.descInput.value
+    });
   }
   handleDescriptionChange = (event)=>{
-    this.setState({"description":this.descInput.value});
+    console.log(this.props);
+    this.props.update({
+      id: this.props.id,
+      title:this.titleInput.value,
+      description:this.descInput.value
+    });
+    
+    let {target} = event;
+    target.style.height = ((target.scrollHeight)-4)+"px";
   }
   render() { 
-    const {title, color, description, bottomBorder} = this.state;
+    const {title, color, description, bottomborder} = this.props;
 
     const colorBlockStyle = color?{"backgroundColor":`rgba(${color.r},${color.g},${color.b},${color.a})`}:{};
 
     return (  
-      <div className="legend-item" {...this.props}>
-        <div className="color-title">
-          <div className="color" style={{...colorBlockStyle}} onClick={()=>alert("colorpicker not implemented")}></div>
-          <input className="legend-title" value={title} onChange={this.handleTitleChange} ref={(node)=>this.titleInput = node}/>
+      <div className="legend-item" style={{...this.props.style}}>
+        {color && 
+          <div 
+            className="color" 
+            style={{...colorBlockStyle}} 
+            onClick={()=>alert("colorpicker not implemented")}>
+          </div>
+        }
+        <div className="legend-info">
+          {this.props.update?
+            <input 
+              className="legend-title" 
+              value={title} 
+              onChange={this.handleTitleChange} 
+              ref={(node)=>this.titleInput = node}/>
+            :
+            <div className="legend-title">{title}</div>
+          }
+          {this.props.update?
+          <textarea 
+            className="legend-description" 
+            value={description} 
+            onChange={this.handleDescriptionChange} 
+            ref={(node)=>this.descInput = node}/>
+          :
+          <div className="legend-description">
+            {description}
+          </div>
+        }
         </div>
-        <textarea className="legend-description" value={description} onChange={this.handleDescriptionChange} ref={(node)=>this.descInput = node}/>
-        {bottomBorder && <div style={{"borderBottom":"solid rgba(0,0,0,.2) 1px", "marginRight":"20%", "marginLeft":"10%", marginBottom:10}}></div>}
+        
+        {bottomborder && <div style={{"borderBottom":"solid rgba(0,0,0,.2) 1px", "marginRight":"20%", "marginLeft":"10%", marginBottom:10}}></div>}
       </div>
     )
   }
