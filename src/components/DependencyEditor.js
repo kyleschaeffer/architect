d8a85115957c61b62c66a8b3a899e27fd11327d4
David Burgess <david@spirito.us>
2018-07-02 08:53:48 -0400

    Styling dependency lists so you can see where I'm going with this

--- src/components/DependencyEditor.js
+++ src/components/DependencyEditor.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class DependencyEditor extends Component {
-  state = {}
+  state = {
+    
+  }
   render() { 
     return ( 
       <div style={{backgroundColor:"#EFEFEFEF"}}>
         <h3 style={{textAlign:"center"}}>Dependencies</h3>
         <div style={{display:"flex"}}>
-          <select multiple style={{flex:1}}/>
-          <select multiple style={{flex:1}}/>
-          <select multiple style={{flex:1}}/>
+          <div style={{flex:1, margin:2}}>
+            <label htmlFor="">Lists</label>
+            <select multiple style={{width:"100%"}}>
+            </select>
+          </div>
+          <div style={{flex:1, margin:2}}>
+            <label htmlFor="">Content Types</label>
+            <select multiple style={{width:"100%"}}>
+            </select>
+          </div>
+          <div style={{flex:1, margin:2}}>
+            <label htmlFor="">Site Columns</label>
+            <select multiple style={{width:"100%"}}>
+            </select>
+          </div>
         </div>
       </div> 
     )
